75493b182c8b52f5c459f70ed36bffc3e108d09e
Yasuaki Watanabe <yasuaki640@gmail.com>
2022-11-07 22:35:33 +0900

    part 1

--- src/main.ts
+++ src/main.ts
@@ -1,37 +1,28 @@
-/**
- * Some predefined delay values (in milliseconds).
- */
-export enum Delays {
-  Short = 500,
-  Medium = 2000,
-  Long = 5000,
+import * as readline from "readline";
+
+async function* readInputs(prompt: string) {
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+
+  try {
+    for (; ;) {
+      yield new Promise((resolve) => rl.question(prompt, resolve));
+    }
+  } finally {
+    rl.close();
+  }
 }
 
-/**
- * Returns a Promise<string> that resolves after a given time.
- *
- * @param {string} name - A name.
- * @param {number=} [delay=Delays.Medium] - A number of milliseconds to delay resolution of the Promise.
- * @returns {Promise<string>}
- */
-function delayedHello(
-  name: string,
-  delay: number = Delays.Medium
-): Promise<string> {
-  return new Promise((resolve: (value?: string) => void) =>
-    setTimeout(() => resolve(`Hello, ${name}`), delay)
-  );
+async function main() {
+  for await (const input of readInputs("db > ")) {
+    if (input == ".exit") {
+      process.exit();
+    } else {
+      console.log(`Unrecognized command ${input}.\n`);
+    }
+  }
 }
 
-// Please see the comment in the .eslintrc.json file about the suppressed rule!
-// Below is an example of how to use ESLint errors suppression. You can read more
-// at https://eslint.org/docs/latest/user-guide/configuring/rules#disabling-rules
-
-// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
-export async function greeter(name: any) { // eslint-disable-line @typescript-eslint/no-explicit-any
-  // The name parameter should be of type string. Any is used only to trigger the rule.
-  return await delayedHello(name, Delays.Long);
-}
-
-const greet = await delayedHello("sdf");
-console.log(greet)
+main();
