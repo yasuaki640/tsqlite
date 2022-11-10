1e6b3bf8ffe43ff0f61f5f984dd78ddcc0ae4df7
Yasuaki Watanabe <yasuaki640@gmail.com>
2022-11-11 08:56:38 +0900

    part2

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,41 @@
 import * as readline from "readline";
 
-const readInputs = async function* (prompt: string) {
+const MetaCommandSuccess = Symbol();
+const MetaCommandUnrecognizedCommand = Symbol();
+type MetaCommandResult = typeof MetaCommandSuccess | typeof MetaCommandUnrecognizedCommand;
+
+const PrepareSuccess = Symbol();
+const PrepareUnrecognizedCommand = Symbol();
+type PrepareResult = typeof PrepareSuccess | typeof PrepareUnrecognizedCommand;
+
+const StatementInsert = Symbol();
+const StatementSelect = Symbol();
+type StatementType = typeof StatementInsert | typeof StatementSelect;
+
+interface Statement {
+  type: StatementType;
+}
+
+const doMetaCommand: (input: string) => MetaCommandResult = (input: string) => {
+  if (input.includes(".exit")) {
+    process.exit();
+  } else {
+    return MetaCommandUnrecognizedCommand;
+  }
+};
+
+const prepareStatement: (input: string) => [PrepareResult, Statement?] = (input: string) => {
+  if (input.startsWith("insert ")) {
+    return [PrepareSuccess, { type: StatementInsert }];
+  }
+  if (input === "select") {
+    return [PrepareSuccess, { type: StatementSelect }];
+  }
+
+  return [PrepareUnrecognizedCommand];
+};
+
+const readInputs = async function* (prompt: string): AsyncGenerator<string> {
   const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
@@ -8,20 +43,48 @@ const readInputs = async function* (prompt: string) {
 
   try {
     for (; ;) {
-      yield new Promise((resolve) => rl.question(prompt, resolve));
+      yield new Promise<string>((resolve) => rl.question(prompt, resolve));
     }
   } finally {
     rl.close();
   }
 };
 
-const main = async () => {
+const executeStatement: (statement: Statement) => void = (statement: Statement) => {
+  switch (statement.type) {
+    case (StatementInsert):
+      console.log("This is where we would do an insert.");
+      break;
+    case (StatementSelect):
+      console.log("This is where we would do an select.");
+      break;
+  }
+};
+
+const main: () => void = async () => {
   for await (const input of readInputs("db > ")) {
-    if (input == ".exit") {
-      process.exit();
-    } else {
-      console.log(`Unrecognized command ${input}.\n`);
+    if (input.startsWith(".")) {
+      switch (doMetaCommand(input)) {
+        case (MetaCommandSuccess):
+          continue;
+        case(MetaCommandUnrecognizedCommand):
+          console.log(`Unrecognized command ${input}`);
+          continue;
+      }
     }
+
+    const [res, statement] = prepareStatement(input);
+
+    switch (res) {
+      case (PrepareSuccess):
+        break;
+      case (PrepareUnrecognizedCommand):
+        console.log(`Unrecognized keyword at start of ${input}`);
+        continue;
+    }
+
+    executeStatement(statement);
+    console.log("Executed.");
   }
 };
 
